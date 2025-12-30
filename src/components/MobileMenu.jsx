62e13ad1cb850db4e88a573f10da77bf764fe6df
mabel <mabelare293@gmail.com>
2025-12-30 13:15:06 +0100

    fixed hamburger menu

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -7,14 +7,49 @@ export default function MobileMenu({ onClose }) {
 
       <div className="space-y-6 text-3xl font-bold">
         <p>Shop all</p>
-        <p>About</p>
+        <a
+          href="https://store.figma.com/pages/about"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="block hover:underline"
+        >
+          About
+        </a>
       </div>
 
       <div className="mt-16 space-y-4 text-sm">
-        <p>FAQ</p>
-        <p>Contact us</p>
-        <p>Privacy Policy</p>
-        <p>Terms of Service</p>
+        <a
+          href="https://store.figma.com/pages/faq"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="block hover:underline"
+        >
+          FAQ
+        </a>
+        <a
+          href="https://store.figma.com/pages/about#contact"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="block hover:underline"
+        >
+          Contact us
+        </a>
+        <a
+          href="https://store.figma.com/pages/privacypolicy"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="block hover:underline"
+        >
+          Privacy Policy
+        </a>
+        <a
+          href="https://store.figma.com/pages/terms-of-service"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="block hover:underline"
+        >
+          Terms of Service
+        </a>
         <p>Account</p>
       </div>
     </div>
